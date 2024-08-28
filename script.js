94da348e2e64369bcbff7ccfed6a10f42f7df978
lowften <135197187+fdlyirhm@users.noreply.github.com>
2024-08-29 03:00:26 +0700

    upload pertama

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".sosmed i").forEach((sosmed) => {
+    sosmed.addEventListener("mouseenter", () => {
+        sosmed.classList.remove("ph");
+        sosmed.classList.add("ph-fill");
+    });
+    sosmed.addEventListener("mouseleave", () => {
+        sosmed.classList.remove("ph-fill");
+        sosmed.classList.add("ph");
+    });
+});
