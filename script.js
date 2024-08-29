8c04d9b2957e0518ac9450263d324ac721abb211
lowften <135197187+fdlyirhm@users.noreply.github.com>
2024-08-29 21:20:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.querySelectorAll(".sosmed i").forEach((sosmed) => {
         sosmed.classList.add("ph");
     });
 });
+
+const actionLink = document.querySelectorAll(".items .items-action");
+
+actionLink.forEach((action) => {
+    action.addEventListener("click", (e) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
+
+        document.getElementById("toast").innerHTML = `
+        <div class="toast-container">
+                <div class="toast">
+                    <p>Link <strong>${action.parentElement.innerText}</strong>  berhasil disalin!</p>
+                </div>
+        </div>`;
+
+        setTimeout(() => {
+            document.querySelector("#toast .toast-container")
+            .classList.add("toast-gone");
+        }, 300);
+
+        setTimeout(() => {
+            document.querySelector("#toast .toast-container").remove();
+        }, 2000)
+    });
+});
